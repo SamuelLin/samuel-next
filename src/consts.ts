b3f24fd2d4949156bc1803cab6bfa5c4ec732eb5
Samuel Lin <samuelhsnu@gmail.com>
2025-06-13 16:26:23 +0800

    feat: aligh css style and some md format

--- src/consts.ts
+++ src/consts.ts
@@ -3,8 +3,8 @@ import type { Site, Metadata, Socials } from "@/types";
 export const SITE: Site = {
   NAME: "Samuel Lin",
   EMAIL: "samuelhsnu@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 1,
+  NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
@@ -36,9 +36,5 @@ export const SOCIALS: Socials = [
   { 
     NAME: "github",
     HREF: "https://github.com/SamuelLin"
-  },
-  { 
-    NAME: "website",
-    HREF: "https://www.samuel-lin.com",
   }
 ];
