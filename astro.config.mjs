e7024b6678f1791b8cfcf481e7d842af1dfd4c82
Samuel Lin <samuelhsnu@gmail.com>
2025-10-04 10:07:41 +0800

    feat: 上線前準備

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    partytown({ config: { forward: ['dataLayer.push', 'gtag'] } }),
+  ],
   site: 'https://samuelhsnu.com'
 })
